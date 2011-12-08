6d623b4eb875e9050114cc046e2c3a39c65c5aa7
whoshallsucceed <whoshallsucceed@gmail.com>
2011-12-08 14:53:15 +0100

    IMP Add the creation of a symbolic link for redmine configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,6 +56,7 @@ namespace :deploy do
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/environments/production.rb #{release_path}/config/environments/production.rb"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/configuration.yml #{release_path}/config/configuration.yml"
   end  
 end
 
